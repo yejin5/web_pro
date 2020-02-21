bbf23ae5ea623f7ec84936211b04366cf44b7dd4
ohyejin1117 <ohyejin1117@gmail.com>
2020-02-21 15:22:33 +0900

    Use hidden tag

--- pro07/src/sec01/ex02/MemberServlet.java
+++ pro07/src/sec01/ex02/MemberServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member")
+//@WebServlet("/member")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
