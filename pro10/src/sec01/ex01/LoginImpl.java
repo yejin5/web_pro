29014ff557fcbe4bb46dd353996e2492e67bccad
ohyejin1117 <ohyejin1117@gmail.com>
2020-02-21 17:00:15 +0900

    calculte login user's number by HttpsessionBindingListner

--- pro10/src/sec01/ex01/LoginImpl.java
+++ pro10/src/sec01/ex01/LoginImpl.java
@@ -1,4 +1,4 @@
-package sec04.ex01;
+package sec01.ex01;
 
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
--- pro10/src/sec01/ex01/LoginTest.java
+++ pro10/src/sec01/ex01/LoginTest.java
@@ -1,4 +1,4 @@
-package sec04.ex01;
+package sec01.ex01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
