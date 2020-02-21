5408c48c8f23af0da7fb8793e04a2284c920df41
ohyejin1117 <ohyejin1117@gmail.com>
2020-02-21 16:58:29 +0900

    calculte login user's number by HttpSessionBindingListner

---
+++ pro10/src/sec01/ex01/LoginTest.java
@@ -0,0 +1,42 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginTest extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+
+		LoginImpl loginUser = new LoginImpl(user_id, user_pw);
+		if (session.isNew()) {
+			session.setAttribute("loginUser", loginUser);
+		}
+
+		out.println("<head>");
+		out.println("<script  type='text/javascript'>");
+		out.println("setTimeout('history.go(0);', 5000)");
+		out.println("</script>");
+		out.println("</head>");
+		out.println("<html><body>");
+		out.println("ID : " + loginUser.user_id + "<br>");
+		out.println("Total number of now : " + LoginImpl.total_user + "<br>");
+		out.println("</body></html>");
+
+	}
+
+}
