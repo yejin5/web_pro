c8c14dd382a071286ed67ae8d01e9beb6487dbe5
ohyejin1117 <ohyejin1117@gmail.com>
2020-02-21 15:55:42 +0900

    popup test

--- pro08/src/sec02/ex01/GetCookieValue.java
+++ pro08/src/sec02/ex01/GetCookieValue.java
@@ -1,40 +1,32 @@
 package sec02.ex01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GetCookieValue
- */
+
 @WebServlet("/get")
 public class GetCookieValue extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public GetCookieValue() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out=response.getWriter();
+	      Cookie[] allValues=request.getCookies();
+	      for(int i=0; i<allValues.length;i++){
+	         if(allValues[i].getName().equals("cookieTest")){
+		      out.println("<h2>This is cookie : "+URLDecoder.decode(allValues[i].getValue(),"utf-8"));
+		  }
+	      }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
